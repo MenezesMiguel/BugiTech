7f168b2d9a8703d3664ba7fd0076a488c81e9cbb
Miguel Menezes <miguel@MacBook-Air-de-Miguel.local>
2024-11-23 01:34:32 -0300

    Adiciona o componente footer

---
+++ src/components/footer.js
@@ -0,0 +1,102 @@
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import {
+  IoLogoInstagram,
+  IoLogoWhatsapp,
+  IoLogoYoutube,
+} from "react-icons/io5";
+
+export default function Footer() {
+  return (
+    <footer className="bg-white py-8 border-t h-auto">
+      <div className="w-full max-w-[1200px] flex flex-col md:flex-row justify-between items-start px-4 py-2 mx-auto">
+        {/* Coluna 1: Logo e Descrição */}
+        <div className="flex-1 max-w-[40%] text-gray-400 text-justify text-[14px]">
+          <Image
+            src="/images/logoHeader2.png"
+            alt="Next.js logo"
+            priority
+            width={200}
+            height={1}
+          />
+          <p>
+            {" "}
+            Desde 2021 trazendo os melhores reviews e as melhores promoções, aqui você paga barato, sem "PROMOFAKE".
+            
+            Não deixe de conhecer nosso trabalho também no YouTube
+          </p>
+        </div>
+
+        {/* Coluna 2: Suporte */}
+        {/* <div className="flex-1">
+          <h3 className="text-lg font-bold mb-4">Suporte</h3>
+          <ul className="space-y-2">
+            <li>
+              <a
+                href="#"
+                className="text-orange-500 hover:text-orange-600 transition"
+              >
+                Fale conosco
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-orange-500 hover:text-orange-600 transition"
+              >
+                Termos de uso
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-orange-500 hover:text-orange-600 transition"
+              >
+                Política de privacidade
+              </a>
+            </li>
+          </ul>
+        </div> */}
+
+        {/* Coluna 3: Redes Sociais */}
+        <div className="flex flex-col px-[8px] py-[16px] justify-end text-gray-600">
+          <h3 className="text-lg font-bold mb-4">Nossas redes sociais</h3>
+          <div className="flex space-x-4">
+            <a
+              href="https://www.youtube.com/c/BugiTech"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IoLogoYoutube
+                size={30}
+                className="text-red-500 hover:text-red-600 transition"
+              />
+            </a>
+            <a
+              href="https://bit.ly/3AvOcAM"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IoLogoWhatsapp
+                size={30}
+                className="text-green-500 hover:text-green-600 transition"
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/bugi.tech/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IoLogoInstagram
+                size={30}
+                className="text-pink-500 hover:text-pink-600 transition"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- src/components/header.js
+++ src/components/header.js
@@ -2,44 +2,65 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-
 import { FaSearch } from "react-icons/fa";
 
-const Header = () => {
-  const [search, setSearch] = useState("");
+export default function Header() {
+  const [areSearching, setAreSearching] = useState(false);
 
-  const handleSearchEdit = (event) => {
-    setSearch(event.target.value)
-    console.log(search)
-  }
+  const handleSearchToggle = () => {
+    setAreSearching((prev) => !prev);
+  };
 
   return (
-    <header className="bg-white flex justify-center row w-full h-28 text-white text-center">
-      <div className="w-full max-w-[1200px] flex row items-center justify-between px-4 py-2">
-        <Image
-          src="/images/logoHeader2.png"
-          alt="Next.js logo"
-          priority
-          width={250}
-          height={1}
-        />
-        <div className="flex items-center bg-white border border-gray-300 rounded-full px-4 py-2 shadow-sm w-[28%] min-w-[230px] hidden sm:flex">
+    <header className="bg-white flex justify-center items-center w-full h-28 text-white text-center">
+      <div className={`w-full max-w-[1200px] flex items-center px-4 py-2 ${areSearching ? "justify-center" : "justify-between"}`}>
+        {/* Renderiza a imagem apenas quando não está pesquisando */}
+        {!areSearching && (
+          <Image
+            src="/images/logoHeader2.png"
+            alt="Next.js logo"
+            priority
+            width={250}
+            height={1}
+          />
+        )}
+
+        {/* Campo de busca em telas grandes (sempre visível) */}
+        <div className="hidden sm:flex items-center bg-white border border-gray-300 rounded-full px-4 py-2 shadow-sm w-[28%] min-w-[230px]">
           <input
             type="text"
-            placeholder="Busque sua promoção"
+            placeholder="Buscar oferta"
             className="flex-grow outline-none text-gray-600 placeholder-gray-400"
-            onChange={handleSearchEdit}
           />
           <button className="cursor-pointer">
             <FaSearch className="text-gray-400 ml-2" />
           </button>
         </div>
-        <button className="block sm:hidden cursor-pointer">
-          <FaSearch className="text-gray-400 ml-6 text-2xl" />
-        </button>
+
+        {/* Lupa e campo de busca em telas pequenas */}
+        <div className="sm:hidden flex items-center w-full">
+          {areSearching ? (
+            <div className="flex items-center bg-white border border-gray-300 rounded-full px-4 py-2 shadow-sm w-full justify-left">
+              <input
+                type="text"
+                placeholder="Busque sua promoção"
+                className="flex-grow outline-none text-gray-600 placeholder-gray-400"
+              />
+              <button className="cursor-pointer" onClick={handleSearchToggle}>
+                <FaSearch className="text-gray-400 ml-2" />
+              </button>
+            </div>
+          ) : (
+            <button
+              className="cursor-pointer flex w-[100%] justify-end"
+              onClick={handleSearchToggle}
+            >
+              <FaSearch className="text-gray-400 ml-6 text-2xl" />
+            </button>
+          )}
+        </div>
       </div>
     </header>
   );
-};
+}
 
-export default Header;
